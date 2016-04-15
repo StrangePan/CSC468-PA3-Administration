1b2892006376f45bde16d7fece2f21f03baf18e0
UrsaLongsight <austin.rotert@mines.sdsmt.edu>
2016-04-15 15:25:46 -0600

    Added some comments to explain what code is doing, as well as adding a small check to make the code a bit safer.

--- includes/template.php
+++ includes/template.php
@@ -1,15 +1,29 @@
 <?php
 
+/**********************************************************
+* Global variables that will hold the extracted portions
+* of the HTML file
+**********************************************************/
 $headContents = "";
 $pageTitle = "";
 $bodyContents = "";
 
+/**********************************************************
+* Extracts the head and title sections, as well as the
+* body portion. Void return.
+**********************************************************/
 function extractContents($page)
 {
     extractHeadAndTitle($page);
     extractBody($page);
 }
 
+/**********************************************************
+* Extracts the head and title sections of the HTML file,
+* if either or both of them exist. Extracted portions are
+* stored in global variables for files that include this
+* one. Void return.
+**********************************************************/
 function extractHeadAndTitle($page)
 {
     global $headContents;
@@ -18,19 +32,25 @@ function extractHeadAndTitle($page)
     $pattern = "#<head.*?>(.*?(<title.*?>(.*?)</title.*?>).*?|.*)</head.*?>#is";
     $matches = array();
 
-    preg_match($pattern, $page, $matches);
-
-    $headContents = trim($matches[1], "\r\n");
-    
-    if(isset($matches[3]))
+    if(preg_match($pattern, $page, $matches))
     {
-        $pageTitle = trim($matches[3]);
-        $titlePattern = "#$matches[2]#";
+        $headContents = trim($matches[1], "\r\n");
+        
+        if(isset($matches[3]))
+        {
+            $pageTitle = trim($matches[3]);
+            $titlePattern = "#$matches[2]#";
 
-        $headContents = preg_replace($titlePattern, '', $headContents);
+            $headContents = preg_replace($titlePattern, '', $headContents);
+        }
     }
 }
 
+/**********************************************************
+* Extracts the body section of the HTML file, assuming one
+* is present. Extracted portion is stored in a global
+* variables for files that include this one. Void return.
+**********************************************************/
 function extractBody($page)
 {
     global $bodyContents;
@@ -38,9 +58,10 @@ function extractBody($page)
     $pattern = "#<body.*?>(.*?)</body.*?>#is";
     $matches = array();
 
-    preg_match($pattern, $page, $matches);
-
-    $bodyContents = trim($matches[1], "\r\n");
+    if(preg_match($pattern, $page, $matches))
+    {
+        $bodyContents = trim($matches[1], "\r\n");
+    }
 }
 
 ?>
