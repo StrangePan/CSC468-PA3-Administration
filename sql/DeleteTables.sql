4625c20007a5b8e526f2443a2f02394c63c1fe7e
Deaboy <danieleandrus@gmail.com>
2016-04-22 10:44:03 -0600

    Added comments and indexes to SQLite tables

--- sql/DeleteTables.sql
+++ sql/DeleteTables.sql
@@ -1,15 +1,31 @@
+-- Drop tables and indexes in the reverse order they were created in
+-- CreateTables.sql
+
 DROP TABLE GroupMembers;
 
 DROP TABLE GroupPermissions;
 
-DROP TABLE UserPermissions;
+DROP INDEX Groups_Name_ind;
 
 DROP TABLE Groups;
 
+
+DROP TABLE UserPermissions;
+
+DROP INDEX Users_Name_ind;
+
 DROP TABLE Users;
+
+
 
 DROP TABLE FilePermissions;
 
+DROP INDEX Permissions_Name_ind;
+
 DROP TABLE Permissions;
 
+DROP INDEX Files_Name_ind;
+
 DROP TABLE Files;
+
+
